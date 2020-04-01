ce68f091172d69675b0633de0eb15d1801fdbe7b
Basile Paradis <basileparadis@HAL-9000.local>
2020-04-01 17:58:26 -0400

    Définition des méthodes de lecture d'un texte

--- lecture.py
+++ lecture.py
@@ -1,10 +1,28 @@
-def televerser_fichier():
-    pass
+import re
 
 
-def lire_texte():
-    pass
+def televerser_fichier(chemin, encodage):
+    file = open(chemin, mode='r', encoding=encodage)
+    return file
 
 
-def liste_mots():
-    pass
+def lire_texte(fichier):
+    liste_mots = []
+    for line in fichier.readlines():
+        words = line.split()
+        for word in words:
+            word = normaliser(word)
+            if word is not "":
+                liste_mots.append(word)
+    return liste_mots
+
+
+def normaliser(mot):
+    mot = mot.lower()
+    mot = re.sub('.*\'', '', mot)
+    mot = mot.translate({ord(c): None for c in ".,!?:;«»"})
+    return mot
+
+
+def liste_mots(chemin, encodage):
+    return lire_texte(televerser_fichier(chemin, encodage))
